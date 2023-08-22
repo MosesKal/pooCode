3b5c5942b5090dbe354ff98ca99c665cb09eac2c
MosesK <mosesziongo@gmail.com>
2023-08-22 10:57:36 +0200

    compte epargne

--- classes/Client/CompteEpargne.php
+++ classes/Client/CompteEpargne.php
@@ -1,10 +1,36 @@
 <?php
-
-
 /**
  * Compte avec taux d'interets
  */
 class CompteEpargne extends Compte
 {
-    private $taux_interets;
+    private $tauxInterets;
+    
+
+    public function __construct(string $nom, float $montant, int $taux)
+    {
+        parent::__construct($nom, $montant);
+
+        $this->tauxInterets = $taux;
+    }
+
+    /**
+     * Get the value of taux_interets
+     */ 
+    public function getTauxInterets(): int
+    {
+        return $this->tauxInterets;
+    }
+
+    /**
+     * Set the value of taux_interets
+     *
+     * @return  self
+     */ 
+    public function setTauxInterets($tauxInterets):self
+    {
+        $this->tauxInterets = $tauxInterets;
+
+        return $this;
+    }
 }
