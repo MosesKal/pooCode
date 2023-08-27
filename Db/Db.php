c469c8624037ec6a7d9f8a1c14c053b857530ecb
MosesK <mosesziongo@gmail.com>
2023-08-27 21:09:25 +0200

    singleton connexion db

--- Db/Db.php
+++ Db/Db.php
@@ -24,17 +24,26 @@ Class Db extends PDO
     {
         //DSN de connexion
         $_dns = 'msql:dbname='.self::DBNAME.';host='.self::DBHOST;
-
         //on appelle le constructeur de la class PDO
-
         try{
             parent::__construct($_dns, self::DBUSER, self::DBPASS);
 
             $this->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
-            
+            $this->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+            $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
         }catch(PDOException $e){
             die($e->getMessage());
         }
 
+    }
+
+
+    public static function getInstance()
+    {
+        if(self::$instance === null){
+            self::$instance = new self();
+        }
+        return self::$instance;
     }
 }
